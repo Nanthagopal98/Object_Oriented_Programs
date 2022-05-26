47bcdeacb6605603d7545f0367939ffdad7c65a5
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-26 19:16:55 +0530

    [Nantha] - Stock inventory Added

--- JSON Inventory Management/JSON Inventory Management/Programe.cs
+++ JSON Inventory Management/JSON Inventory Management/Programe.cs
@@ -10,11 +10,20 @@ namespace JSON_Inventory_Management
     {
         public static void Main()
         {
-            inventoryManagement json = new inventoryManagement();
-            json.group(@"D:\Bridgelabz\.Net\Object_Oriented_Programs\JSON Inventory Management\JSON Inventory Management\Inventory.json");
-
-            //stockManagement manage = new stockManagement();
-            //manage.convert(@"D:\Bridgelabz\.Net\Object_Oriented_Programs\JSON Inventory Management\JSON Inventory Management\syock.json");
+            Console.Write("Enter 1 to run Food inventory\n");
+            Console.Write("Enter 2 to run Stock inventory");
+            int select = Convert.ToInt32(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    inventoryManagement json = new inventoryManagement();
+                    json.group(@"D:\Bridgelabz\.Net\Object_Oriented_Programs\JSON Inventory Management\JSON Inventory Management\Inventory.json");
+                    break;
+                case 2:
+                    stockManagement manage = new stockManagement();
+                    manage.convert(@"D:\Bridgelabz\.Net\Object_Oriented_Programs\JSON Inventory Management\JSON Inventory Management\syock.json");
+                    break;
+            }
         }
     }
 }
